66cea679c2c7a3b53c373c5f41360a6d35e147bb
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-07-18 16:05:10 +0200

    fix image warning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
                     alt={"Movie Poster"}
                     width={100}
                     height={100}
+                    style={{ width: "auto", height: "auto" }}
                   />
                 )}
                 <div>
