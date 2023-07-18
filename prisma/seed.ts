f2b49cd6e15fafe27b00ce7d396be805f397eb14
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-07-18 12:26:43 +0200

    seed database

---
+++ prisma/seed.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client"
+import { movies } from "../data/movies"
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.movies.createMany({
+    data: movies,
+  })
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
