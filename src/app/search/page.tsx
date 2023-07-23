cfd3cf43eff2ea3d6b01cacd0f13194b05eec096
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-07-23 13:56:13 +0200

    refactor movie search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { gql, useLazyQuery } from "@apollo/client"
-import type { Movie } from "@prisma/client"
 import { useEffect, useState } from "react"
 import MovieCard from "@/components/MovieCard"
 
@@ -29,86 +28,106 @@ const movieQuery = gql`
 
 export default function SearchPage() {
   const [getMovie, { loading, error, data }] = useLazyQuery(movieQuery)
-  const [inputValue, setInputValue] = useState("")
   const [titleToSearch, setTitleToSearch] = useState("")
-  const [movie, setMovie] = useState<Movie>()
 
-  function handleChange(e) {
-    setInputValue(e.target.value)
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault()
-    setTitleToSearch(inputValue)
+  function handleSubmit(newMovieTitle) {
+    setTitleToSearch(newMovieTitle)
   }
 
   useEffect(() => {
     if (!titleToSearch) return
-    getMovie({ variables: { title: titleToSearch } }).then((result) => {
-      if (result.data) setMovie(result.data.movie[0])
-    })
+    getMovie({ variables: { title: titleToSearch } })
   }, [titleToSearch])
+
+  return (
+    <main className="p-10 flex flex-col items-center gap-4">
+      <SearchMovieForm titleToSearch={titleToSearch} onSubmit={handleSubmit} />
+      <MovieInfo loading={loading} error={error} data={data} />
+    </main>
+  )
+}
+
+function SearchMovieForm({ titleToSearch, onSubmit }) {
+  const [inputValue, setInputValue] = useState(titleToSearch ?? "")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    onSubmit(inputValue)
+  }
 
   function handleSelect(newMovieTitle) {
     setInputValue(newMovieTitle)
-    setTitleToSearch(newMovieTitle)
+    onSubmit(newMovieTitle)
   }
 
   return (
-    <main>
-      <form
-        className="p-10 flex flex-col items-center gap-4"
-        onSubmit={handleSubmit}
-      >
-        <label htmlFor="movieTitle-input" className="text-lg font-bold">
-          Movie Title
-        </label>
-        <small>
-          Try{" "}
-          <button
-            className="btn-invisible"
-            type="button"
-            onClick={() => handleSelect("Pulp Fiction")}
-          >
-            "Pulp Fiction"
-          </button>
-          {", "}
-          <button
-            className="btn-invisible"
-            type="button"
-            onClick={() => handleSelect("Frozen Planet")}
-          >
-            "Frozen Planet"
-          </button>
-          {", or "}
-          <button
-            className="btn-invisible"
-            type="button"
-            onClick={() => handleSelect("Django Unchained")}
-          >
-            "Django Unchained"
-          </button>
-        </small>
-        <div className="flex gap-2">
-          <input
-            placeholder="Movie Title"
-            id="movieTitle-input"
-            name="movieTitle"
-            value={inputValue}
-            onChange={handleChange}
-          />
-          <button className="btn" type="submit">
-            Submit
-          </button>
-        </div>
-        {loading && <div>Loading...</div>}
-        {error && <div>{error.message}</div>}
-        {movie && (
-          <div className="max-w-[350px]">
-            <MovieCard movie={movie} />
-          </div>
-        )}
-      </form>
-    </main>
+    <form className="flex flex-col items-center gap-4" onSubmit={handleSubmit}>
+      <label htmlFor="movieTitle-input" className="text-lg font-bold">
+        Movie Title
+      </label>
+      <small>
+        Try{" "}
+        <button
+          className="btn-invisible"
+          type="button"
+          onClick={() => handleSelect("Pulp Fiction")}
+        >
+          "Pulp Fiction"
+        </button>
+        {", "}
+        <button
+          className="btn-invisible"
+          type="button"
+          onClick={() => handleSelect("Frozen Planet")}
+        >
+          "Frozen Planet"
+        </button>
+        {", or "}
+        <button
+          className="btn-invisible"
+          type="button"
+          onClick={() => handleSelect("Django Unchained")}
+        >
+          "Django Unchained"
+        </button>
+      </small>
+      <div className="flex gap-2">
+        <input
+          placeholder="Movie Title"
+          id="movieTitle-input"
+          name="movieTitle"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </div>
+    </form>
   )
+}
+
+function MovieInfo({ loading, error, data }) {
+  if (loading) return <div className="info-box card">Loading...</div>
+  if (error) return <div className="info-box card">{error.message}</div>
+  if (data) {
+    const movie = data.movie[0]
+    if (movie) {
+      return (
+        <div className="max-w-[350px]">
+          <MovieCard movie={movie} />
+        </div>
+      )
+    } else {
+      return (
+        <div className="info-box card">No movie found with this title.</div>
+      )
+    }
+  } else {
+    return (
+      <div className="info-box card">
+        Please enter the title of the movie and click submit!
+      </div>
+    )
+  }
 }
